3eb8a4aa5127f51301ae11cf2c07cb0da701d3a4
daoudi elhassan <daoudiha6@gmail.com>
2024-08-14 23:33:31 +0100

    vercel deploy

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import { HomeComponent } from '@/components/component/HomeComponent';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen p-4 md:p-8 lg:p-16 bg-background">
       <div className="container mx-auto flex flex-col items-center justify-center flex-grow">
         <HomeComponent />
+        <SpeedInsights />
       </div>
     </main>
   );
