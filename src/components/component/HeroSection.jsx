349954787312e1c26f36807fb7c1c74bd4d1499c
daoudi elhassan <daoudiha6@gmail.com>
2024-08-28 18:10:53 +0100

    "Removed Image import from 'next/image' and replaced Image component with img tag in HeroSection.jsx"

--- src/components/component/HeroSection.jsx
+++ src/components/component/HeroSection.jsx
@@ -2,8 +2,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import { Input } from "@/components/ui/input";
-import Image from 'next/image';
-
 
 export function HeroSection() {
   const [isFloatingVisible, setFloatingVisible] = useState(false);
@@ -88,7 +86,7 @@ export function HeroSection() {
 
           {/* Hero Image */}
           <div className="hero-image w-full xl:w-1/2 flex justify-center xl:justify-end">
-            <Image
+            <img
               src="/pictures/IT.png"
               alt="IT Club logo, a vibrant and colorful representation of the community"
               className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-full object-cover rounded-2xl shadow-2xl"
