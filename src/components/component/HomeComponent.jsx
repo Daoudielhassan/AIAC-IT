8794bf4a6ced5bf10fd0818bc83d12e3b2ba741b
daoudi elhassan <daoudiha6@gmail.com>
2024-08-28 17:55:24 +0100

    "Deleted EventsSection and FeaturedProjectsSection components, removed Carousel component and its related files, updated Header component to use Next Image, and modified HomeComponent to no longer import EventsSection and FeaturedProjectsSection."

--- src/components/component/HomeComponent.jsx
+++ src/components/component/HomeComponent.jsx
@@ -1,7 +1,5 @@
 import { Header } from "./Header";
 import { HeroSection } from "./HeroSection";
-import { EventsSection } from "./EventsSection";
-import { FeaturedProjectsSection } from "./FeaturedProjectsSection";
 import { MeetTheTeamSection } from "./MeetTheTeamSection";
 import { Contact } from "./contact";
 export function HomeComponent() {
