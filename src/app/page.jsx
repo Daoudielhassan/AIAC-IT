92fd3c9b66c0554b8e195174432984c73416a78b
daoudi elhassan <daoudiha6@gmail.com>
2024-11-26 23:33:26 +0100

    "Updated import and export statements for PartnersSection component"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,8 +5,7 @@ import { HeroSection } from "@/components/component/HeroSection";
 import { MeetTheTeamSection } from "@/components/component/MeetTheTeamSection";
 import { Contact } from "@/components/component/contact";
 import { Cell } from "@/components/component/cells";
-import PartnersSection from '@/components/component/PartnersSection';
-export default function Home() {
+import { PartnersSection } from '@/components/component/PartnersSection';export default function Home() {
   return (
     <main className="flex flex-col min-h-screen p-4 md:p-3 lg:p-10 bg-background">
       <Header />
