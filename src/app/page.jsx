64e5b2880a5c2a7f32343d249bc4f633d63ad947
daoudi elhassan <daoudiha6@gmail.com>
2024-11-26 23:39:25 +0100

    "Added 'use client' directive to page.jsx, updated PartnersSection import/export, and removed trailing newline in PartnersSection.jsx"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import { Header } from "@/components/component/Header";
@@ -5,7 +7,9 @@ import { HeroSection } from "@/components/component/HeroSection";
 import { MeetTheTeamSection } from "@/components/component/MeetTheTeamSection";
 import { Contact } from "@/components/component/contact";
 import { Cell } from "@/components/component/cells";
-import { PartnersSection } from '@/components/component/PartnersSection';export default function Home() {
+import { PartnersSection } from '@/components/component/PartnersSection';
+
+export default function Home() {
   return (
     <main className="flex flex-col min-h-screen p-4 md:p-3 lg:p-10 bg-background">
       <Header />
